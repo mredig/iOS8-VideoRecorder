651c48c903eb9c22d07e48415af237e6d1728553
Michael Redig <mredig@gmail.com>
2019-10-02 11:48:09 -0500

    camera permission request

--- VideoRecorder/ViewController.swift
+++ VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -19,7 +20,41 @@ class ViewController: UIViewController {
 		super.viewDidAppear(animated)
 		
 		// TODO: get permission
-		
+		requestCameraPermission()
+	}
+
+	private func requestCameraPermission() {
+		// TODO: get permission: show cam if we have it
+		// TODO: error conditions with lack of permission
+		let status = AVCaptureDevice.authorizationStatus(for: .video)
+
+		switch status {
+		case .notDetermined:
+			// we have not asked yet
+			requestCameraAccess()
+		case .restricted:
+			// parental controls limit
+			fatalError("Please inform user they can't use app cuz their parents suck")
+		case .denied:
+			// user set
+			fatalError("Please inform user they can't use app - tell them to enable in settings")
+		case .authorized:
+			showCamera()
+		@unknown default:
+			break
+		}
+	}
+
+	private func requestCameraAccess() {
+		AVCaptureDevice.requestAccess(for: .video) { granted in
+			if granted == false {
+				fatalError("User said no")
+			} else {
+				DispatchQueue.main.async {
+					self.showCamera()
+				}
+			}
+		}
 	}
 	
 	private func showCamera() {
