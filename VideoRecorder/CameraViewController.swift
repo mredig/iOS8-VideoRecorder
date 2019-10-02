2bc2d7996eb8118472771998a475824f233f1e9e
Michael Redig <mredig@gmail.com>
2019-10-02 12:12:52 -0500

    choose best camera

--- VideoRecorder/CameraViewController.swift
+++ VideoRecorder/CameraViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+
+	lazy private var captureSession = AVCaptureSession()
 
 	@IBOutlet var recordButton: UIButton!
 	@IBOutlet var cameraView: CameraPreviewView!
@@ -17,8 +20,24 @@ class CameraViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+
+		setupCamera()
 	}
 
+	private func setupCamera() {
+		// get camera
+		let camera = bestCamera()
+		// set up settings
+		// set capture session on camera view
+	}
+
+	private func bestCamera() -> AVCaptureDevice {
+		if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+			return device
+		}
+
+		fatalError("No cameras")
+	}
 
 	@IBAction func recordButtonPressed(_ sender: Any) {
 
